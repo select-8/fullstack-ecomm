090e22567676878bb54d5239ecce8298249fda32
Liam O'Connor <liamoconnor8481@gmail.com>
2019-09-05 12:06:31 +0100

    added heroku to allowed hosts

--- onlinestore/settings.py
+++ onlinestore/settings.py
@@ -34,7 +34,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.environ.get('DEBUG')
 
-ALLOWED_HOSTS = [os.environ.get('localhost')]
+ALLOWED_HOSTS = [os.environ.get('localhost'), 'fullstack-ecom.herokuapp.com']
 
 
 # Application definition
